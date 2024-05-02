54bce32f0fbe53eec64ff84cbf0305ffe6e90acc
Sabine Freiman <sabinefreiman@gmail.com>
2024-05-02 19:34:43 -0300

    CRUD Categoria

---
+++ MarketMargoAPI/Controllers/CategoriaController.cs
@@ -0,0 +1,104 @@
+﻿using MarketMargoAPI.Models;
+using MarketMargoAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketMargoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoriaController: ControllerBase
+    {
+        private readonly ConnectionDB _dbContext;
+
+        public CategoriaController(ConnectionDB dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetCategorias()
+        {
+            CategoriaService categoriaService = new CategoriaService(_dbContext);
+
+            IEnumerable<Categoria> categorias = await categoriaService.GetCategorias();
+
+            return Ok(categorias);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Categoria>> GetCategoria(int id)
+        {
+            CategoriaService categoriaService = new CategoriaService(_dbContext);
+
+            Categoria? categoria = await categoriaService.GetCategoriaById(id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return categoria;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CriarCategoria(NovaCategoria novaCategoria)
+        {
+            try
+            {
+                CategoriaService categoriaService = new CategoriaService(_dbContext);
+
+                Categoria categoria = new Categoria();
+                categoria.Nome = novaCategoria.Nome;
+                categoria.Data_criacao = DateTime.Now;
+                categoria.Data_modificacao = DateTime.Now;
+                categoria.Ativo = true;
+
+                await categoriaService.CriarCategoria(categoria);
+
+                return Ok(categoria);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategoria(int id, AtualizarCategoria atualizarCategoria)
+        {
+            CategoriaService categoriaService = new CategoriaService(_dbContext);
+
+            Categoria categoria = new Categoria();
+            categoria.Nome = atualizarCategoria.Nome;
+            categoria.Ativo = atualizarCategoria.Ativo;
+
+            var existingCategoria = await _dbContext.TbCategoria.FindAsync(id);
+
+            if (existingCategoria == null)
+            {
+                return NotFound();
+            }
+
+            await categoriaService.AtualizarCategoria(existingCategoria, categoria);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategoria(int id)
+        {
+            CategoriaService categoriaService = new CategoriaService(_dbContext);
+
+            var categoria = await _dbContext.TbCategoria.FindAsync(id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            await categoriaService.DeletarCategoria(categoria);
+
+            return NoContent();
+        }
+    }
+}
--- MarketMargoAPI/Services/CategoriaService.cs
+++ MarketMargoAPI/Services/CategoriaService.cs
@@ -1,6 +1,48 @@
-﻿namespace MarketMargoAPI.Services
+﻿using MarketMargoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketMargoAPI.Services
 {
     public class CategoriaService
     {
+        private readonly ConnectionDB _dbContext;
+
+        public CategoriaService(ConnectionDB dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Categoria>> GetCategorias()
+        {
+            return await _dbContext.TbCategoria.ToListAsync();
+        }
+
+        public async Task<Categoria?> GetCategoriaById(int id)
+        {
+            return await _dbContext.TbCategoria.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<Categoria> CriarCategoria(Categoria categoria)
+        {
+            _dbContext.TbCategoria.Add(categoria);
+            await _dbContext.SaveChangesAsync();
+
+            return categoria;
+        }
+
+        public async Task AtualizarCategoria(Categoria existingCategoria, Categoria categoria)
+        {
+            existingCategoria.Nome = categoria.Nome;
+            existingCategoria.Data_modificacao = DateTime.Now;
+
+            _dbContext.TbCategoria.Update(existingCategoria);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeletarCategoria(Categoria categoria)
+        {
+            _dbContext.TbCategoria.Remove(categoria);
+            await _dbContext.SaveChangesAsync();
+        }
     }
 }
